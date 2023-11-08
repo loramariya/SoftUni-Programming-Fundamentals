696c509ce9ee2a5aba58ab50f9be95ed32c786d2
loramariya <loramaria.raynova@gmail.com>
2023-11-08 15:52:03 +0200

    Upload Exercises

--- 07.AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
+++ 07.AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charOccurrences = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+
+                if (character == ' ') ;
+                {
+                    continue;
+                }
+
+                if (!charOccurrences.ContainsKey(character))
+                {
+                    charOccurrences.Add(character, 0);
+                }
+
+                charOccurrences[character]++;
+            }
+
+            foreach (var occurrence in charOccurrences)
+            {
+                Console.WriteLine($"{occurrence.Key} -> {occurrence.Value}");
+            }
+
         }
     }
 }
