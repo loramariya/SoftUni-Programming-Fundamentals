e0ab1838c3e6b25ab0aabcc68e09bff427458246
loramariya <loramaria.raynova@gmail.com>
2023-11-22 22:08:20 +0200

    Upload Exercises

---
+++ 08.TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (string username in usernames)
+            {
+                if (username.Length < 3 || username.Length > 16)
+                {
+                    continue;
+                }
+
+                if (IsValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        private static bool IsValid(string username)
+        {
+            return username.All(x => char.IsLetterOrDigit(x) 
+            || x == '-' 
+            || x == '_');
+        }
+    }
+}
